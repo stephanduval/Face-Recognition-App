d579800b402dc2fdfebacd69d0622dcf6ea518b9
Stephan DuVal <stephan.duval@gmail.com>
2021-06-15 12:36:58 -0600

    Initial Commit created react app, navigation bar and customized app.js

---
+++ face-recognition/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navigation from './components/navigation/navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        
+      <Navigation />
+      /*
+      <logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+        */
+      }
+    </div>
+  );
+}
+
+export default App;
